066bedc42025be50e73ab13cce49a6b318641de8
Kanak Wanjari <wanjarikanak@gmail.com>
2024-05-27 15:05:01 +0530

    Commit

--- a.py
+++ a.py
@@ -4,16 +4,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    name = "Kanak"
+    name = "monali"
     return render_template('index.html', Name = name)
 
-# @app.route('/about')
-# def about():
-#     return 'This is the about page'
+@app.route('/about')
+def about():
+    return 'This is the about page'
 
-# @app.route('/contact')
-# def contact():
-#     return 'This is the contact page'
+@app.route('/contact')
+def contact():
+    return 'This is the contact page'
 
 if __name__ == '__main__':
     app.run(debug=True)

ee1e66e45d6d2391d02b9da9a8c9d92b8a01bc81
Kanak Wanjari <wanjarikanak@gmail.com>
2024-05-27 11:16:16 +0530

    first commit

---
+++ a.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    name = "Kanak"
+    return render_template('index.html', Name = name)
+
+# @app.route('/about')
+# def about():
+#     return 'This is the about page'
+
+# @app.route('/contact')
+# def contact():
+#     return 'This is the contact page'
+
+if __name__ == '__main__':
+    app.run(debug=True)

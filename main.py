2c32b2573292d6e0e3a74200ea6d9772678ac9a3
Kanak Wanjari <wanjarikanak@gmail.com>
2024-06-04 15:16:37 +0530

    Commit

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/index")
 def home():
     return render_template('index.html')
 
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+@app.route("/post")
+def post():
+    return render_template('post.html')
 
 @app.route("/contact")
 def contact():
